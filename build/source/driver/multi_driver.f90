f276a8901407f5b1bab29dc717003d07a2013016
luckymichael <michael.ou@hotmail.com>
2016-03-31 20:05:58 -0700

    print elapsed time fix

--- build/source/driver/multi_driver.f90
+++ build/source/driver/multi_driver.f90
@@ -863,7 +863,7 @@ contains
  call date_and_time(values=ctime2)
  
  ! calculate the elapsed time (wall clock time)
- elp_s = (ctime2(8)-ctime1(8))*.001 + (ctime2(7)-ctime1(7)) + (ctime2(6)-ctime1(6))*60. + (ctime2(5)-ctime1(5))*3600.
+ elp_s = (ctime2(8)-ctime1(8))*.001_sp + (ctime2(7)-ctime1(7)) + (ctime2(6)-ctime1(6))*60_sp + (ctime2(5)-ctime1(5))*3600_sp
 
  ! check if it's same day simulation and calculate how many days
  if (ctime2(1) > ctime1(1) .or. ctime2(2) > ctime1(2) .or. ctime2(3) > ctime1(3)) then
@@ -877,23 +877,23 @@ contains
   if ((mod(ctime1(1),4)==0 .and. .not. mod(ctime1(1),100)==0) .or. (mod(ctime1(1),400)==0)) days1(2) = 29
   if ((mod(ctime2(1),4)==0 .and. .not. mod(ctime2(1),100)==0) .or. (mod(ctime2(1),400)==0)) days2(2) = 29
 
-  if (ctime1(1) > 1) elp_d = elp_d - sum(days1(1:(ctime1(2)-1)))
-  elp_d = elp_d - ctime1(3) + 1
+  if (ctime1(2) > 1) elp_d = elp_d - sum(days1(1:(ctime1(2)-1)))
+  elp_d = elp_d - ctime1(3) 
 
-  if (ctime2(1) > 1) elp_d = elp_d + sum(days2(1:(ctime2(2)-1)))
+  if (ctime2(2) > 1) elp_d = elp_d + sum(days2(1:(ctime2(2)-1)))
   elp_d = elp_d + ctime2(3)
 
-  elp_s = elp_s + elp_d * 86400.
+  elp_s = elp_s + elp_d * 86400_sp
  end if
  
  ! print initial and final date and time
  write(outunit,"(A,I4,'-',I2.2,'-',I2.2,4x,I2,':',I2.2,':',I2.2,'.',I3.3)"),'initial date/time = ',ctime1(1:3),ctime1(5:8)
  write(outunit,"(A,I4,'-',I2.2,'-',I2.2,4x,I2,':',I2.2,':',I2.2,'.',I3.3)"),'  final date/time = ',ctime2(1:3),ctime2(5:8)
  ! print elapsed time
- write(outunit,"(/,A,1PG15.7,A)"),                                          '     elapsed time = ', elp_s,        ' s'
- write(outunit,"(A,1PG15.7,A)"),                                            '       or           ', elp_s/60.,    ' m'
- write(outunit,"(A,1PG15.7,A)"),                                            '       or           ', elp_s/3600.,  ' h'
- write(outunit,"(A,1PG15.7,A)"),                                            '       or           ', elp_s/86400., ' d'
+ write(outunit,"(/,A,1PG15.7,A)"),                                          '     elapsed time = ', elp_s,          ' s'
+ write(outunit,"(A,1PG15.7,A)"),                                            '       or           ', elp_s/60_sp,    ' m'
+ write(outunit,"(A,1PG15.7,A)"),                                            '       or           ', elp_s/3600_sp,  ' h'
+ write(outunit,"(A,1PG15.7,A)"),                                            '       or           ', elp_s/86400_sp, ' d'
  ! stop with message
  print*,'FORTRAN STOP: '//trim(message)
  stop
